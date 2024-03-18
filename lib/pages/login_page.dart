a362cd2c4e1b68d9cbc62b6874f0f4cf66e541bf
ArsenijDKS <ars.manag@yandex.ru>
2024-03-18 20:19:50 +0300

    modified:   lib/pages/login_page.dart     modified:   Login Page, Text Field Widget

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/components/my_textfield.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Logo
+            Icon(
+              Icons.lock_open_rounded,
+              size: 100,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+
+            const SizedBox(height: 25),
+
+            // Message, app slogan
+            Text(
+              "Food Delivery App",
+              style: TextStyle(
+                fontSize: 16,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+            ),
+
+            const SizedBox(height: 25),
+
+            // Email textfield
+            MyTextField(
+              controller: emailController,
+              hintText: "Email",
+              obscureText: false,
+            ),
+
+            // Password textfield
+
+            // Sign in button
+
+            // Register
+          ],
+        ),
+      ),
+    );
   }
 }
